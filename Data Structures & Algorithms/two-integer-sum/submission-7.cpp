47398b91bcbc26dfff0c083803a799ff3bf2e977
lenguyenminhtriet2077-blip <lenguyenminhtriet2077@gmail.com>
2026-07-19 20:21:34 +0700

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> numMap;
+
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int difference = target - nums[i];
+
+            if (numMap.count(difference))
+            {
+                return {numMap[difference], i};
+            }
+
+            numMap[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
